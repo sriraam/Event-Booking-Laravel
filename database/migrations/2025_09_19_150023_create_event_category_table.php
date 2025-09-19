0218a7d8eb78d1fe21e953e985e2b0a410136156
www-data <www-data@s5430203.elf.ict.griffith.edu.au>
2025-09-20 01:34:26 +1000

    Updated table migration files

---
+++ database/migrations/2025_09_19_150023_create_event_category_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('event_category', function (Blueprint $table) {
+            $table->foreignId('event_id')->references('id')->on('events')->cascadeOnDelete();
+            $table->foreignId('category_id')->references('id')->on('categories')->cascadeOnDelete();
+            $table->primary(['event_id','category_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('event_category');
+    }
+};
