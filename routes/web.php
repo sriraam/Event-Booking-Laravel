880b867d5f5462cadefc459d6b08f1e93b37dcfb
www-data <www-data@s5430203.elf.ict.griffith.edu.au>
2025-09-21 00:27:41 +1000

    Implmented user authentication

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
 });
+*/
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/',function(){ 
+    return view('auth.login'); 
+});
+
+Route::redirect('*/', '/login')->name('home');
+require __DIR__.'/auth.php';
